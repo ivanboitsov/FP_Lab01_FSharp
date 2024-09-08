eac6e2f0d6cd162db596ba1e1ce1acb7b94a9f8d
Ivan <ivanboitsov386@gmail.com>
2024-09-08 22:46:04 +0300

    Вторая часть лабораторной работы
    - Fix
    * Метод итераций

--- FP_Lab01_FSharp/equations.fs
+++ FP_Lab01_FSharp/equations.fs
@@ -1,28 +1,69 @@
 ﻿module equations
 
-// Определите функции для решение алгебраических уравнений
+// Метод деления пополам (дихотомия)
+let rec dichotomy f (a:float) (b:float) =
+    let eps = 0.000001
+    let xn = (a + b) / 2.
+    let fa = f a
+    let fb = f b
 
-let dichotomy f a b = 0.
+    if abs(f xn) < eps then
+        xn
+    elif fa < fb then
+        if (f xn) < 0. then dichotomy f xn b else dichotomy f a xn
+    else
+        if (f xn) > 0. then dichotomy f xn b else dichotomy f a xn
 
-let iterations phi x0 = 0.
+// Метод итерации
+let rec iterations phi x0 = 
+    let eps = 0.1
 
-let newthon f f' x0 = 0.
-// используйте функцию 'iterations'
+    if abs(x0 - (phi x0)) < eps then
+        x0
+    else
+        let next = phi x0
+        iterations phi next
+
+// Метод Ньютона
+let rec newthon f f' x0 = 
+    let phi x: float = x - (f x ) / (f' x)
+    // Используя метод итерации
+    iterations phi x0
+
 
 // Решите 3 уравнения (начиная со своего номера варианта) с использованием 3-х методов
-let f1 = ...
-let f2 = ...
-let f3 = ...
+// Оригинальные уравнения
+let f1 x: float = 3. * x - 4. * System.Math.Log x - 5. // #23
 
-let f1' = ...
-let f2' = ...
-let f3' = ...
+let f2 x: float = 
+    System.Math.Cos (2./x) - 2. * System.Math.Sin (1./x) + (1./x) // #24
 
-let phi1 = ...
-let phi2 = ...
-let phi3 = ...
+let f3 x: float =
+    System.Math.Sqrt (1. - 0.4 * (x ** 2.))  - System.Math.Asin x // #25
+
+// Первая производная
+let f1' x: float = 3. - 4./x // #23
+
+let f2' x: float = 
+    ((2. * System.Math.Cos(1./x) + 2. * System.Math.Sin(2./x))/(x**2.)) - (1./(x**2)) // #24
+
+let f3' x: float = 
+    (-2.*x)/(5. * System.Math.Sqrt(1. - ((2.*(x**2.))/5.))) - (1. / (System.Math.Sqrt(1. - (x ** 2.)))) // #25
+
+// Выражение x из первоначального уравнения
+let phi1 x : float =  (4. * System.Math.Log x + 5.) / 3.
+
+let phi2 x : float = 1./(2.*sin(1./x) - cos(2./x))
+
+let phi3 x : float = System.Math.Sqrt((1. - (System.Math.Asin x)**2.) / 0.4)
 
 let main = 
-    printfn "%10.5f  %10.5f  %10.5f" (dichotomy f1 0. 1.) (iterations phi1 0.) (newthon f1 f1' 1.)
-    printfn "%10.5f  %10.5f  %10.5f" (dichotomy f2 0. 1.) (iterations phi2 0.) (newthon f2 f2' 1.)
-    printfn "%10.5f  %10.5f  %10.5f" (dichotomy f3 0. 1.) (iterations phi3 0.) (newthon f3 f3' 1.)
+    // printfn "%10.5f  %10.5f  %10.5f" (dichotomy f1 2. 4.) (iterations phi1 3.) (newthon f1 f1' 3.)
+    // printfn "%10.5f  %10.5f  %10.5f" (dichotomy f2 1. 2.) (iterations phi2 1.5) (newthon f2 f2' 1.5)
+    // printfn "%10.5f  %10.5f  %10.5f" (dichotomy f3 0. 1.) (iterations phi3 0.5) (newthon f3 f3' 0.5)
+
+
+     printfn "%10.5f" (iterations phi1 3.)
+     // printfn "%10.5f" (iterations phi2 1.5)
+     printfn "%10.5f" (iterations phi3 0.5)
+main
